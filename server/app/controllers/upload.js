a4a2395d825580afe1ace65eeca5701c7d29711c
chenwenhua <wenhua.chen@versa-ai.com>
2018-08-29 15:41:28 +0800

    图片上传 自动生成uuid

--- server/app/controllers/upload.js
+++ server/app/controllers/upload.js
@@ -1,22 +1,27 @@
 // upload controller
 // const config = require('../config/index.js')()
 const path = require('path')
-const fs = require('fs')
-const upload_service = require('../services/upload')
 const fs_service = require('../services/fs')
+const util = require('../extends/util')
 
 exports.image = async (ctx, next) => {
-  const {imageName, data} = ctx.request.body;
-  const base64Data = data.replace(/^data:image\/\w+;base64,/, '');
+  const {data = ''} = ctx.request.body  
+  let suffix = 'png' // 文件后缀
+  const base64Data = data.replace(/^data:image\/(\w+);base64,/, (result, $1, offset) => {
+    suffix = $1
+    return ''
+  })
   const dataBuffer = new Buffer(base64Data, 'base64')
   const filePath = path.resolve(__dirname, '../../assets/upload/images')
-  const fileName = path.join(filePath, imageName + '.png')
+  let imageName = util.createImgName() + '.' + suffix
+  const fileName = path.join(filePath, imageName)
+
   // 判断文件夹是否存在，不存在则创建
   const mkdirStatus = await fs_service.createDir(filePath)
   if (mkdirStatus) {
     let result
     try {
-      result = await upload_service.writeFile(fileName, dataBuffer)
+      result = await fs_service.writeFile(fileName, dataBuffer)
       ctx.body = {
         result: result
       }
@@ -32,17 +37,24 @@ exports.image = async (ctx, next) => {
 
 // 测试
 exports.test = async (ctx, next) => {
-  const {imageName, data} = ctx.request.body;
-  const base64Data = data.replace(/^data:image\/\w+;base64,/, '');
+  const {data = ''} = ctx.request.body
+  let suffix = 'png' // 文件后缀
+  const base64Data = data.replace(/^data:image\/(\w+);base64,/, (result, $1, offset) => {
+    suffix = $1
+    return ''
+  })
+  console.log('suffix', suffix)
   const dataBuffer = new Buffer(base64Data, 'base64')
   const filePath = path.resolve(__dirname, '../../assets/demo/images')
-  const fileName = path.join(filePath, imageName + '.png')
+  let imageName = util.createImgName() + '.' + suffix
+  const fileName = path.join(filePath, imageName)
+
   // 判断文件夹是否存在，不存在则创建
   const mkdirStatus = await fs_service.createDir(filePath)
   if (mkdirStatus) {
     let result
     try {
-      result = await upload_service.writeFile(fileName, dataBuffer)
+      result = await fs_service.writeFile(fileName, dataBuffer)
       ctx.body = {
         result: result
       }
--- server/app/services/upload.js
+++ server/app/services/upload.js
@@ -2,18 +2,18 @@
 'use strict'
 const fs = require('fs')
 const upload = {
-  writeFile (fileName, dataBuffer) {
-    // 写入文件
-    return new Promise((resolve, reject) => {
-      fs.writeFile(fileName, dataBuffer, err => {        
-        if (err) {
-          // console.log('fs.writeFile', err)
-          reject(err)
-        } else {
-          resolve({result: 'success'})
-        }
-      })
-    })
-  }
+  // writeFile (fileName, dataBuffer) {
+  //   // 写入文件
+  //   return new Promise((resolve, reject) => {
+  //     fs.writeFile(fileName, dataBuffer, err => {        
+  //       if (err) {
+  //         // console.log('fs.writeFile', err)
+  //         reject(err)
+  //       } else {
+  //         resolve({result: 'success'})
+  //       }
+  //     })
+  //   })
+  // }
 }
 module.exports = upload
