f0d5c991a71cb2e18173ba1a0c446f9d06ec391a
HarryChen0506 <chenwenhua0506@sina.com>
2018-08-29 23:34:05 +0800

    fix 文件大小写

--- admin/src/views/layout/index.vue
+++ admin/src/views/layout/index.vue
@@ -10,7 +10,7 @@
 
 <script>
 // import { Navbar, Sidebar, AppMain } from './components'
-import Sidebar from './components/SidebarOld'
+import Sidebar from './components/Sidebar'
 import Navbar from './components/Navbar'
 import AppMain from './components/AppMain'
 
