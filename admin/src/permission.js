e11b17a6668a9e62c3d1c94cae30acb853461b04
chenwenhua <wenhua.chen@versa-ai.com>
2018-09-10 14:40:40 +0800

    路由权限

--- admin/src/permission.js
+++ admin/src/permission.js
@@ -4,14 +4,33 @@
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css' //这个样式必须引入
 import router from '@/router'
+import util from '@/utils/util'
 import config from '@/config'
-
+const {accessKey = 'accessToken'} = config()
 const {whiteList = []} = config()
+const token = util.LocalStorage.get(accessKey)
 
 router.beforeEach((to, from, next) => {
 	NProgress.start()
   // console.log('white', whiteList)
-  
+  if (token) {
+    // token生成
+    if (to.path === '/login') {
+      next({ path: '/' })
+      NProgress.done() 
+    } else {
+     next()
+    }
+  } else {
+    // 未生成token
+    if (whiteList.indexOf(to.path) !== -1) {
+      // console.log('白名单')
+      next()
+    } else  {
+      next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
+      NProgress.done()
+    }
+  }
   next()
 })
 
